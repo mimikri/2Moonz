fa4b974192f0fc9dc76622e4c455798d3e12acba
kleinhirn <pocco_13@yahoo.de>
2024-11-30 12:11:23 +0100

    fix js

--- scripts/base/tooltip.js
+++ scripts/base/tooltip.js
@@ -1,50 +1,51 @@
 /**
- *  2Moons
+ *  spacepunks
  *   by Jan-Otto Kröpke 2009-2016
+ *   by mimikri 2023
  *
  * For the full copyright and license information, please view the LICENSE
  *
- * @package 2Moons
- * @author Jan-Otto Kröpke <slaver7@gmail.com>
+ * @package spacepunks
+ * @author mimikri
  * @copyright 2009 Lucky
  * @copyright 2016 Jan-Otto Kröpke <slaver7@gmail.com>
  * @licence MIT
- * @version 1.8.0
- * @link https://github.com/jkroepke/2Moons
+ *@version 0.0.1
+ * @link https://github.com/mimikri/spacepunks
  */
 
-function tiptool() {
-
-	$(".tooltip").off('mouseenter').on('mouseenter', function (e) {
+$(document).ready(function () {
+	$(".tooltip").live({
+		mouseenter : function (e) {
 			var tip = $('#tooltip');
 			tip.html($(this).attr('data-tooltip-content'));
 			tip.show();
-		});
-	$(".tooltip").off('mouseleave').on('mouseleave', function () {
+		},
+		mouseleave : function () {
 			var tip = $('#tooltip');
 			tip.hide();
-		});
-		$(".tooltip").off('mousemove').on('mousemove', function (e) {
+		},
+		mousemove : function (e) {
 			var tip = $('#tooltip');
-			var mousex = e.pageX + 30;
-			var mousey = e.pageY + 30;
+			var mousex = e.pageX + 20;
+			var mousey = e.pageY + 20;
 			var tipWidth = tip.width();
 			var tipHeight = tip.height();
 			var tipVisX = $(window).width() - (mousex + tipWidth);
 			var tipVisY = $(window).height() - (mousey + tipHeight);
-			if (tipVisX < 30) {
-				mousex = e.pageX - tipWidth - 30;
+			if (tipVisX < 20) {
+				mousex = e.pageX - tipWidth - 20;
 			};
-			if (tipVisY < 30) {
-				mousey = e.pageY - tipHeight - 30;
+			if (tipVisY < 20) {
+				mousey = e.pageY - tipHeight - 20;
 			};
 			tip.css({
 				top : mousey,
 				left : mousex
 			});
-		});
-
-	$(".tooltip_sticky").off('mouseenter').on('mouseenter', function (e) {
+		}
+	});
+	$(".tooltip_sticky").live('mouseenter', function (e) {
 		var tip = $('#tooltip');
 		tip.html($(this).attr('data-tooltip-content'));
 		tip.addClass('tooltip_sticky_div');
@@ -53,11 +54,10 @@ function tiptool() {
 			left : e.pageX - tip.outerWidth() / 2
 		});
 		tip.show();
-		nuscript('tooltip');
 	});
-	$(".tooltip_sticky_div").off('mouseleave').on('mouseleave', function () {
+	$(".tooltip_sticky_div").live('mouseleave', function () {
 		var tip = $('#tooltip');
 		tip.removeClass('tooltip_sticky_div');
 		tip.hide();
 	});
-}
+});
